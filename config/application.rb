26879da2017a110c65e779bd50ab2cc04ddc0392
so2toll <steven.otu@gmail.com>
2014-03-19 00:07:03 -0700

    precompiled commit3

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,7 @@ module SampleApp
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 	  config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+	  # config/application.rb
+config.assets.initialize_on_precompile = false
   end
 end
