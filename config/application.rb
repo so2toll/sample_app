6c0997f21d7ee26f627bb3cb26ddd2bf7b4a9307
so2toll <steven.otu@gmail.com>
2014-03-21 11:06:52 -0700

    slight token change

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,7 @@ module SampleApp
 	  # config/application.rb
 config.assets.initialize_on_precompile = false
 config.secret_token = '3613594cc3054dcada0a0ae4b427493fa7e3459e58dbdf81a960eed90bec56ed71984c497e04abe4
-0a3a5e3805a32b1df375831f32b1fe914a57f712bc7366a9
+0a3a5e3805b32b1df375831f32b1fe914a57f712bc7366a9
 '
 
   end
